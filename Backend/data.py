c0a6011300cc22fd40ea7084a607e24c1cf0e3a8
SymenYang <SymenYang@163.com>
2017-12-22 22:45:53 +0800

    added base struct

---
+++ Backend/data.py
@@ -0,0 +1,54 @@
+import json
+import codecs
+import socket
+import os
+
+class SingleClass(object):  
+    def __new__(cls, *args, **kw):  
+        if not hasattr(cls, '_instance'):  
+            orig = super(SingleClass, cls)  
+            cls._instance = orig.__new__(cls, *args, **kw)  
+        return cls._instance  
+
+class FrogDropData(SingleClass) : 
+    selfIP = "127.0.0.1"
+    broadcasterPort = 3524
+    fileURI = ""
+    fileName = ""
+    receiverList = [] #(IP,userName)
+    userName = ""
+    
+    fileBuffer = ""
+    userNameFile = "userinfo.log"
+
+    def initial(self):
+        '''
+        initial data from self.userNameFile and system info
+        include selfIP,userName
+        '''
+        self.userName = socket.getfqdn(socket.gethostname())
+        self.selfIP = socket.gethostbyname(self.userName)
+        if not os.path.exists(self.userNameFile):
+            userinfofd = codecs.open(self.userNameFile,'w','utf-8')
+            userinfofd.write('{\n\t"userName" : "' + self.userName + '",\n}')
+            userinfofd.close()
+            return
+        userinfofd = codecs.open(self.userNameFile,'r','utf-8')
+        textdata = userinfofd.read()
+        userinfoJson = json.loads(textdata)
+        self.userName = userinfoJson["userName"]
+        if userinfoJson.has_key('broadcasterPort'):
+            self.broadcasterPort = userinfoJson["broadcasterPort"]
+        userinfofd.close()
+
+    def save(self):
+        userinfofd = codecs.open(self.userNameFile,'w','utf-8')
+        tempDic = {}
+        tempDic['userName'] = self.userName
+        tempDic['broadcasterPort'] = self.broadcasterPort
+        userinfofd.write(json.dumps(tempDic))
+        userinfofd.close()
+
+data = FrogDropData()
+data.initial()
+data.save()
---
+++ Backend/protocol.py
@@ -0,0 +1,52 @@
+import json
+
+
+
+class FrogProtocol() :
+    def loadFromString(inputString) :
+        dicData = json.loads(inputString)
+
+        if not ('Method' in dicData and \
+                'Sender' in dicData and \
+                'SenderPort' in dicData and \
+                'Reciver' in dicData and \
+                'ReciverPort' in dicData and \
+                'URI' in dicData and) :
+            return {"error" : "don't have basic component"}
+        
+        if dicData['Method'] == 'PUT' :
+            if not ('UserName' in dicData and \
+                    'Size' in dicData) :
+                return {"error" : "don't have PUT component"}
+            else:
+                return dicData
+
+        if dicData['Method'] == 'REC' :
+            if not ('UserName' in dicData) :
+                return {"error" : "don't have REC component"}
+            else:
+                return dicData
+
+        if dicData['Method'] == 'GET' :
+            if not ('StartPos' in dicData and \
+                    'Size' in dicData) :
+                return {"error" : "don't have GET component"}
+            else:
+                return dicData
+
+        if dicData['Method'] == 'TRS' :
+            if not ('File' in dicData and \
+                    'Size' in dicData) :
+                return {"error" : "don't have TRS component"}
+            else:
+                return dicData
+        
+        return {"error" : "Methods dosen't accepted"}
+
+    def getTrsString(inputDic) :
+        ret = {}
+        acceptList = ['Method','Sender','SenderPort','Reciver','ReciverPort','URI','UserName','Size','StartPos','File']
+        for item in acceptList:
+            if item in inputDic:
+                ret[item] = inputDic[item]
+        return json.dumps(ret)
