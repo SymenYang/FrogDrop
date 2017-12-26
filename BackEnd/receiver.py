ce265bfd3e220284d00957265b0d138f1dbd48c2
SymenYang <SymenYang@163.com>
2017-12-27 02:05:42 +0800

    changed a lot

--- BackEnd/receiver.py
+++ BackEnd/receiver.py
@@ -12,108 +12,110 @@ stopReceive = False
 
 
 def startListen(nextfunc):
-	global stopReceive
-	stopReceive = False
-	thread.start_new_thread(listen, (nextfunc, None))
+    global stopReceive
+    stopReceive = False
+    thread.start_new_thread(listen, (nextfunc, None))
 
 
 def stopListen():
-	global stopReceive
-	print('ready to stop listen')
-	stopReceive = True
+    global stopReceive
+    print('ready to stop listen')
+    stopReceive = True
 
 
 def listen(nextfunc, sth=None):
-	global stopReceive
-	Data = DT.FrogDropData()
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s.bind((Data.selfIP, 36500))
-	print('listening at ' + Data.selfIP + ':36500')
-	while not stopReceive:
-		s.settimeout(5)
-		try:
-			s.listen(5)
-			conn, addr = s.accept()
-			conn.setblocking(1)
-			received = conn.recv(65535).decode('utf-8')
-			receivedData = PT.loadFromString(received)
-			Data.fileURI = receivedData['URI']
-			temp = Data.fileURI.replace('\\', '/')
-			fileSplit = temp.split('/')
-			Data.fileName = fileSplit[len(fileSplit) - 1]
-			Data.fileSize = receivedData['Size']
-			Data.reqName = receivedData['UserName']
-			Data.reqIP = receivedData['Sender']
-			resDic = {"Method": "REC",
-			          "Sender": Data.selfIP,
-			          "SenderPort": 36500,
-			          "Receiver": Data.reqIP,
-			          "ReceiverPort": receivedData['SenderPort'],
-			          "URI": receivedData['URI'],
-			          "UserName": Data.userName}
-			resString = PT.getTrsString(resDic)
-			conn.send(resString.encode())
-			conn.close()
-			if nextfunc != None:
-				nextfunc()
-				return
-		except:
-			pass
-	# print('receive time out')
+    global stopReceive
+    Data = DT.FrogDropData()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((Data.selfIP, 36500))
+    print('listening at ' + Data.selfIP + ':36500')
+    while not stopReceive:
+        try:
+            s.settimeout(5)
+            s.listen(5)
+            conn, addr = s.accept()
+            conn.setblocking(1)
+            received = conn.recv(65535).decode('utf-8')
+            receivedData = PT.loadFromString(received)
+            Data.fileURI = receivedData['URI']
+            temp = Data.fileURI.replace('\\', '/')
+            fileSplit = temp.split('/')
+            Data.fileName = fileSplit[len(fileSplit) - 1]
+            Data.fileSize = receivedData['Size']
+            Data.reqName = receivedData['UserName']
+            Data.reqIP = receivedData['Sender']
+            resDic = {"Method": "REC",
+                      "Sender": Data.selfIP,
+                      "SenderPort": 36500,
+                      "Receiver": Data.reqIP,
+                      "ReceiverPort": receivedData['SenderPort'],
+                      "URI": receivedData['URI'],
+                      "UserName": Data.userName}
+            resString = PT.getTrsString(resDic)
+            conn.send(resString.encode())
+            conn.close()
+            s.close()
+            if nextfunc != None:
+                nextfunc()
+                return
+
+        except:
+            pass
+# print('receive time out')
 
 
 def finishRecon(accept, nextfunc):
-	Data = DT.FrogDropData()
-	size = 1024
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s.connect((Data.reqIP, 36501))
-	reqDic = {"Method": "GET",
-	          "Sender": Data.selfIP,
-	          "SenderPort": 36501,
-	          "Receiver": Data.reqIP,
-	          "ReceiverPort": 36501,
-	          "URI": Data.fileURI,
-	          "StartPos": 0,
-	          "Size": 0}
-	if not accept:
-		s.send(PT.getTrsString(reqDic).encode())
-		data = s.recv(65535).decode('utf-8')
-		s.close()
-		if nextfunc != None:
-			nextfunc()
-		return
-	nowPos = 0
-	filed = open(Data.downloadDir + Data.fileName, 'wb')
-	while nowPos <= Data.fileSize:
-		reqDic['Size'] = size
-		if size + nowPos > Data.fileSize:
-			reqDic['Size'] = Data.fileSize - nowPos
-		reqDic['StartPos'] = nowPos
-		s.send(PT.getTrsString(reqDic).encode())
-		data = s.recv(65535).decode('utf-8')
-		if nowPos == Data.fileSize:
-			break
-		resDic = PT.loadFromString(data)
-		file = resDic['File']
-		finaldata = base64.b64decode(file)
-		filed.write(finaldata)
-		nowPos += len(file)
-		print(str(nowPos) + ' of ' + str(Data.fileSize) + ' received')
-	# s.close()
-	filed.close()
-	if nextfunc != None:
-		nextfunc()
+    Data = DT.FrogDropData()
+    size = 1024
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.connect((Data.reqIP, 36501))
+    reqDic = {"Method": "GET",
+              "Sender": Data.selfIP,
+              "SenderPort": 36501,
+              "Receiver": Data.reqIP,
+              "ReceiverPort": 36501,
+              "URI": Data.fileURI,
+              "StartPos": 0,
+              "Size": 0}
+    if not accept:
+        s.send(PT.getTrsString(reqDic).encode())
+        data = s.recv(65535).decode('utf-8')
+        s.close()
+        if nextfunc != None:
+            nextfunc()
+        return
+    nowPos = 0
+    filed = open(Data.downloadDir + Data.fileName, 'wb')
+    while nowPos <= Data.fileSize:
+        reqDic['Size'] = size
+        if size + nowPos > Data.fileSize:
+            reqDic['Size'] = Data.fileSize - nowPos
+        reqDic['StartPos'] = nowPos
+        s.send(PT.getTrsString(reqDic).encode())
+        data = s.recv(65535).decode('utf-8')
+        if nowPos == Data.fileSize:
+            break
+        resDic = PT.loadFromString(data)
+        file = resDic['File']
+        finaldata = base64.b64decode(file)
+        filed.write(finaldata)
+        nowPos += len(file)
+        print(str(nowPos) + ' of ' + str(Data.fileSize) + ' received')
+    # s.close()
+    filed.close()
+    if nextfunc != None:
+        nextfunc()
 
 
 def accept():
-	finishRecon(True, None)
+    finishRecon(True, None)
 
 
 if __name__ == '__main__':
-	Data = DT.FrogDropData()
-	Data.initial()
-	startListen(accept)
-	Timer = threading.Timer(15, stopListen)
-	Timer.start()
+    Data = DT.FrogDropData()
+    Data.initial()
+    startListen(accept)
+    Timer = threading.Timer(15, stopListen)
+    Timer.start()
--- FrontEnd/frogdrop.py
+++ FrontEnd/frogdrop.py
@@ -5,162 +5,172 @@ from PyQt5.QtWidgets import QMessageBox
 from BackEnd import broadcaster, data, listener, protocol, sender, receiver
 import sys, time, os
 
+sys.path.insert(0, '..')
+
 
 class FrogDrop(QtWidgets.QMainWindow, Ui_FrogDrop):
-	def __init__(self):
-		super(FrogDrop, self).__init__()
-		broadcaster.startBroadcast()
-		listener.startListen()
-		receiver.startListen(self.recv_alert)
+    __received_signal = QtCore.pyqtSignal()
 
-		self.setupUi(self)
-		self.RefreshBtn.clicked.connect(self.refresh)
-		self.ChooseBtn.clicked.connect(self.choose)
-		self.EditBtn.clicked.connect(self.rename)
+    def __init__(self):
+        super(FrogDrop, self).__init__()
+        broadcaster.startBroadcast()
+        listener.startListen()
+        receiver.startListen(self.recv_alert)
 
-		self.LogoPic.setPixmap(QtGui.QPixmap('../images/logo_tmp.jpg'))
-		self.LogoPic.setScaledContents(True)
-		self.HeadPic.setPixmap(QtGui.QPixmap('../images/pika.jpg'))
-		self.HeadPic.setScaledContents(True)
+        self.setupUi(self)
+        self.RefreshBtn.clicked.connect(self.refresh)
+        self.ChooseBtn.clicked.connect(self.choose)
+        self.EditBtn.clicked.connect(self.rename)
+        self.__received_signal.connect(self.show_Message_Box)
 
-		self.refresh()
+        self.LogoPic.setPixmap(QtGui.QPixmap('../images/logo_tmp.jpg'))
+        self.LogoPic.setScaledContents(True)
+        self.HeadPic.setPixmap(QtGui.QPixmap('../images/pika.jpg'))
+        self.HeadPic.setScaledContents(True)
+
+        self.refresh()
+
+    def dropping(self):
+        while True:
+            time.sleep(5)
+            self.refresh()
+
+    def refresh(self):
+        self.HostIP.setText(fd_data.selfIP)
+        self.NameLabel.setText(fd_data.userName)
+
+        self.RecTable.clear()
+        self.RecTable.setColumnCount(2)
+        self.RecTable.setRowCount(len(fd_data.receiverList))
+        self.RecTable.horizontalHeader().setStretchLastSection(True)
+        self.RecTable.setHorizontalHeaderLabels(['IP', 'Nickname'])
+        self.RecTable.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        table_item = QtWidgets.QTableWidgetItem
+
+        for idx, ip in enumerate(fd_data.receiverList):
+            self.RecTable.setItem(idx, 0, table_item(ip))
+            self.RecTable.setItem(idx, 1, table_item(fd_data.receiverList[ip][0]))
+
+    def rename(self):
+        value, ok = QtWidgets.QInputDialog.getText(self, "Edit Nickname", "You want to be seen as...",
+                                                   QtWidgets.QLineEdit.Normal, fd_data.userName)
+        if ok:
+            fd_data.setNewName(value)
+            self.refresh()
+
+    def choose(self):
+        try:
+            cur_row = self.RecTable.currentRow()
+            ip = self.RecTable.item(cur_row, 0).text()
+            print(ip)
+        except:
+            QMessageBox.warning(self, 'WARNING', 'You are sending to ghost:)', QMessageBox.Ok)
+            return
+
+        send_dg = SendDialog(ip)
+        send_dg.show()
+        send_dg.exec_()
+
+    def recv_alert(self):
+        if fd_data.reqIP == fd_data.selfIP:
+            ok = True
+            receiver.finishRecon(ok, self.restart_listen)
+        else:
+            print('here')
+            self.__received_signal.emit()
+            #ok = QMessageBox.question(self, 'New Request',
+            #                          'Do you want to receive %s from %s?' % (fd_data.fileName, fd_data.reqName),
+            #                          QMessageBox.Yes | QMessageBox.No)
 
 
-	def dropping(self):
-		while True:
-			time.sleep(5)
-			self.refresh()
+    def show_Message_Box(self):
+        ok = (QMessageBox.Yes == QMessageBox.question(self,'New Request','Do you want to receive %s from %s?' % (fd_data.fileName, fd_data.reqName),QMessageBox.Yes | QMessageBox.No))
+        receiver.finishRecon(ok, self.restart_listen)
 
-	def refresh(self):
-		self.HostIP.setText(fd_data.selfIP)
-		self.NameLabel.setText(fd_data.userName)
-
-		self.RecTable.clear()
-		self.RecTable.setColumnCount(2)
-		self.RecTable.setRowCount(len(fd_data.receiverList))
-		self.RecTable.horizontalHeader().setStretchLastSection(True)
-		self.RecTable.setHorizontalHeaderLabels(['IP', 'Nickname'])
-		self.RecTable.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
-		table_item = QtWidgets.QTableWidgetItem
-
-		for idx, ip in enumerate(fd_data.receiverList):
-			self.RecTable.setItem(idx, 0, table_item(ip))
-			self.RecTable.setItem(idx, 1, table_item(fd_data.receiverList[ip][0]))
-
-	def rename(self):
-		value, ok = QtWidgets.QInputDialog.getText(self, "Edit Nickname", "You want to be seen as...",
-		                                           QtWidgets.QLineEdit.Normal, fd_data.userName)
-		if ok:
-			fd_data.setNewName(value)
-			self.refresh()
-
-	def choose(self):
-		try:
-			cur_row = self.RecTable.currentRow()
-			ip = self.RecTable.item(cur_row, 0).text()
-			print(ip)
-		except:
-			QMessageBox.warning(self, 'WARNING', 'You are sending to ghost:)', QMessageBox.Ok)
-			return
-
-		send_dg = SendDialog(ip)
-		send_dg.show()
-		send_dg.exec_()
-
-	def recv_alert(self):
-		if fd_data.reqIP == fd_data.selfIP:
-			ok = True
-		else:
-			ok = QMessageBox.question(self, 'New Request',
-			                          'Do you want to receive %s from %s?' % (fd_data.fileName, fd_data.reqName),
-			                          QMessageBox.Yes | QMessageBox.No)
-		receiver.finishRecon(ok, self.restart_listen)
-
-	def restart_listen(self):
-		receiver.startListen(self.recv_alert)
+    def restart_listen(self):
+        receiver.startListen(self.recv_alert)
 
 
 class SendDialog(QtWidgets.QDialog, Ui_SendDialog):
-	def __init__(self, ip):
-		super(SendDialog, self).__init__()
+    def __init__(self, ip):
+        super(SendDialog, self).__init__()
 
-		self.setupUi(self)
-		self.SelectBtn.clicked.connect(self.select_file)
-		self.SendBtn.clicked.connect(self.send)
-		self.ip = ip
-		self.name = fd_data.receiverList[ip][0]
-		self.file = None
-		self.ProgBar.setValue(0)
+        self.setupUi(self)
+        self.SelectBtn.clicked.connect(self.select_file)
+        self.SendBtn.clicked.connect(self.send)
+        self.ip = ip
+        self.name = fd_data.receiverList[ip][0]
+        self.file = None
+        self.ProgBar.setValue(0)
 
-	def select_file(self):
-		path = QtWidgets.QFileDialog.getOpenFileName(self,
-		                                             'Select file',
-		                                             '',
-		                                             'All Files (*)')[0]
-		if path != None:
-			self.file = path
-			if not os.path.exists(self.file):
-				QMessageBox.warning('WARNING', 'You just selected nothing:(', QMessageBox.Ok)
-				return
-		print(path)
-		self.SendLog.setText('Selected ' + self.file.split('/')[-1])
+    def select_file(self):
+        path = QtWidgets.QFileDialog.getOpenFileName(self,
+                                                     'Select file',
+                                                     '',
+                                                     'All Files (*)')[0]
+        if path != None:
+            self.file = path
+            if not os.path.exists(self.file):
+                QMessageBox.warning(self, 'WARNING', 'You just selected nothing:(')
+                return
+        print(path)
+        self.SendLog.setText('Selected ' + self.file.split('/')[-1])
 
-	def send(self):
-		self.SendBtn.setEnabled(False)
+    def send(self):
+        self.SendBtn.setEnabled(False)
 
-		msg = 'Waiting for response...'
-		self.SendLog.setText(msg)
+        msg = 'Waiting for response...'
+        self.SendLog.setText(msg)
 
-		# if sender.acceptFlag is sender.SD_NO:
-		# 	self.SendLog.setText('He/She said no:(')
-		# 	time.sleep(5)
-		# 	self.SendBtn.setEnabled(True)
-		# 	return
+        # if sender.acceptFlag is sender.SD_NO:
+        # 	self.SendLog.setText('He/She said no:(')
+        # 	time.sleep(5)
+        # 	self.SendBtn.setEnabled(True)
+        # 	return
 
-		msg = 'Sending %s to %s...' % (self.file.split('/')[-1], self.name)
-		self.SendLog.setText(msg)
+        msg = 'Sending %s to %s...' % (self.file.split('/')[-1], self.name)
+        self.SendLog.setText(msg)
 
-		# sender.startSend(self.ip, self.file, sender.sendFile)
+        # sender.startSend(self.ip, self.file, sender.sendFile)
 
-		# while not sender.stopFlag:
-		# 	self.ProgBar.setValue(fd_data.sentSize * 1.0 / fd_data.fileSize * 100)
+        # while not sender.stopFlag:
+        # 	self.ProgBar.setValue(fd_data.sentSize * 1.0 / fd_data.fileSize * 100)
 
-		sender.startSend(self.ip, self.file, sender.sendFile)
-		# self.work_thread = SendThread(self.ip, self.file)
-		# self.work_thread.trigger.connect(self.send_complete)
-		# self.work_thread.start()
-		# self.work_thread.wait()
+        sender.startSend(self.ip, self.file, sender.sendFile)
+        # self.work_thread = SendThread(self.ip, self.file)
+        # self.work_thread.trigger.connect(self.send_complete)
+        # self.work_thread.start()
+        # self.work_thread.wait()
 
-		sender.stopFlag = False
-		sender.acceptFlag = sender.SD_WAIT
+        sender.stopFlag = False
+        sender.acceptFlag = sender.SD_WAIT
 
-		self.SendLog.setText('File sent.')
-		self.SendBtn.setEnabled(True)
+        self.SendLog.setText('File sent.')
+        self.SendBtn.setEnabled(True)
 
-
-	def send_complete(self, flag):
-		if flag:
-			self.SendBtn.setEnabled(True)
-			self.SendLog.setText('File sent.')
-		else:
-			self.SendBtn.setEnabled(True)
-			self.SendLog.setText('Sending abort.')
+    def send_complete(self, flag):
+        if flag:
+            self.SendBtn.setEnabled(True)
+            self.SendLog.setText('File sent.')
+        else:
+            self.SendBtn.setEnabled(True)
+            self.SendLog.setText('Sending abort.')
 
 
 class SendThread(QtCore.QThread):
-	trigger = QtCore.pyqtSignal(bool)
+    trigger = QtCore.pyqtSignal(bool)
 
-	def __init__(self, ip, file):
-		super(SendThread, self).__init__()
-		self.ip = ip
-		self.file = file
+    def __init__(self, ip, file):
+        super(SendThread, self).__init__()
+        self.ip = ip
+        self.file = file
 
-	def run(self):
-		try:
-			sender.startSend(self.ip, self.file, sender.sendFile)
-			self.trigger.emit(True)
-		except:
-			self.trigger.emit(False)
+    def run(self):
+        try:
+            sender.startSend(self.ip, self.file, sender.sendFile)
+            self.trigger.emit(True)
+        except:
+            self.trigger.emit(False)
 
 
 fd_data = data.FrogDropData()
@@ -168,7 +178,6 @@ fd_data.initial()
 
 app = QtWidgets.QApplication(sys.argv)
 frogdrop = FrogDrop()
-
 
 frogdrop.show()
 sys.exit(app.exec())
